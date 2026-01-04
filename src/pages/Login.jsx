dd30107a6e8a049c0528223e0b6379c173f706a0
BGC <uthmanajanaku@gmail.com>
2026-01-04 15:49:46 +0100

    fix: revert button loading spinners to standard loader

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,9 @@
-import { Eye, EyeOff, Lock, Mail, User2Icon } from "lucide-react";
+import { Eye, EyeOff, Loader2, Lock, Mail, User2Icon } from "lucide-react";
 import React from "react";
 import api from "../configs/api.js";
 import { useDispatch } from "react-redux";
 import { login } from "../app/features/authSlice";
 import toast from "react-hot-toast";
-import LogoIcon from "../components/LogoIcon";
 
 const Login = () => {
   const dispatch = useDispatch();
